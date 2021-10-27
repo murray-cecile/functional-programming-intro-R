7db8c352f69c49a096cb3eeb7960a2dc90934e6e
Cecile Murray <29072007+murray-cecile@users.noreply.github.com>
2021-10-27 09:45:40 -0400

    edits + final pdf slides

--- intro_to_functions.R
+++ intro_to_functions.R
@@ -14,7 +14,7 @@ library(tidyverse)
 
 # define a function
 square <- function(a) {
-  return(a*2)
+  return(a * 2)
 }
 
 # one way to repeatedly call a function
@@ -30,16 +30,21 @@ numbers_to_square <- seq(0, 5)
 #========== LOOP
 
 
-# use a loop to call the function on each number in the list
+# use a loop to call the function on each number in the list and print result
 for(num in numbers_to_square) {
   print(square(num))
 }
 
-# but loops in R are slow, avoid using them if you can!
+# loops are less abstract, but may not be the best tool:
+# - lots of bookkeeping about what you're looping over
+# - sometimes slower 
 
 #==========  USING APPLY() FUNCTIONS IN BASE R
 
-# this produces a numeric vector
+# this produces a numeric vector 
+sapply(numbers_to_square, function(x) { square(x) })
+
+# this produces a numeric vector (more concise syntax)
 sapply(numbers_to_square, square)
 
 # this produces a list 
@@ -48,7 +53,11 @@ lapply(numbers_to_square, square)
 #========== USING PURRR::MAP()
 
 # this will produce a numeric vector just like sapply
+purrr::map_dbl(numbers_to_square, function(x) { square(x) })
+
+# this is the more concise, conventional way to write that
 purrr::map_dbl(numbers_to_square, ~ square(.))
+
 
 #==========================================================#
 # USING MAP() WITH DATA FRAMES
@@ -78,14 +87,17 @@ for(col in colnames(pov19)) {
   print(class(pov19[[col]]))
 }
 
-# we could use lapply, but it's less readable than the loop
+# we could use lapply, but it's also hard to read this code
 lapply(
   seq(ncol(pov19)),
   function(x) {class(pov19[[colnames(pov19)[[x]]]])}  
 )
 
 # a dataframe is a list of columns, so with purrr::map we can do this:
-pov19 %>% map_chr(class)
+pov19 %>% 
+  map_chr(function(x) { class(x) } )
+
+# more concise syntax, same thing
 
 #==========================================================#
 # MORE COMPLEX EXAMPLE WITH MAP()
@@ -100,9 +112,11 @@ pov19 %>% map_chr(class)
 
 create_rate_df <- function(year) {
 
+    # read in the data
     read_csv(
       str_c("data/poverty_", year, ".csv")
     ) %>% 
+    # transform it to long
     pivot_longer(
       cols = contains("poor"),
       names_to = "pop_type",
@@ -110,8 +124,9 @@ create_rate_df <- function(year) {
     ) %>% 
     mutate(
       year = year, # create new variable to store year
-      pct_poor = (poor_ct / pov_univ) * 100
+      pct_poor = (poor_ct / pov_univ) * 100 # compute pct in poverty
     ) %>% 
+    # transform back to wide
     select(-poor_ct) %>% 
     pivot_wider(
       names_from = pop_type,
@@ -124,7 +139,7 @@ create_rate_df(2019)
 
 # let's do this for 4 years of data!
 
-output <- map_df(
+output <- map_dfr(
   seq(2016, 2019, 1),
   ~ create_rate_df(.)
 )
